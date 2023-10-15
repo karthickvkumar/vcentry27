1df4f711e0cb217313a09e412502e137846ae80d
karthickvkumar <karthickvelraj@live.in>
2023-10-15 13:31:39 +0530

    commit message

---
+++ HTML/coding.js
@@ -0,0 +1,18 @@
+/*syntax - Variable definition or creation (To pass value to javascript)
+var variable_name = value;
+
+var - Keyword (Resered word with some action)
+variable_name - Temp storage (Allowed character for Variable name a-z, A-Z, _ , $)
+value - Primitive / Object type */
+
+// Number - Any Numberical Value from the range of Positive, Negative, Decimal, Fractional
+
+var rollnumber = 14668;
+
+// String - Any Text Content (content should be enclosed inside single or double quoutes)
+
+var emailid = "karthick@gmail.com";
+
+// Boolean - true or false
+
+var result = true;
