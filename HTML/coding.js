9f011c01027796b1b1bcb57e36f1a632373c71e6
karthickvkumar <karthickvelraj@live.in>
2023-10-21 11:19:45 +0530

    commit message

--- HTML/coding.js
+++ HTML/coding.js
@@ -16,3 +16,85 @@ var emailid = "karthick@gmail.com";
 // Boolean - true or false
 
 var result = true;
+
+// Null - Non Exist data 
+
+// undefined - A variable will be defined but its value will not be definied.
+
+var futureValue;
+
+// Object - It is a collection of key and value pair (collection of properties)
+// syntax
+// var variable_name = {
+//   key : value,
+//   key : value,
+//   key : value
+// }; 
+
+// key - similar to variable name 
+// value -  Primitive or Object type of data
+
+var firstName = "mr.abc";
+var lastName = "yzx";
+var rollNumber = 414552;
+var standard = "XI";
+var contactNumber = 4152556;
+
+
+var studentInfomation = {
+  firstName : "mr.abc",
+  lastName : "yzx",
+  rollNumber : 414552,
+  standard : "XI",
+  contactNumber : 4152556
+}
+
+// Array - It is List of Data, it should have similar data type 
+// syntax 
+// var variable_name = [value1, value2, ... , valueN];
+// value - Primitive or Object type of data
+
+var studentList = [
+  {
+    firstName : "mr.abc",
+    lastName : "yzx",
+    rollNumber : 414552,
+    standard : "XI",
+    contactNumber : 4152556
+  },
+  {
+    firstName : "mr.abc",
+    lastName : "yzx",
+    rollNumber : 414552,
+    standard : "XI",
+    contactNumber : 4152556
+  },
+  {
+    firstName : "mr.abc",
+    lastName : "yzx",
+    rollNumber : 414552,
+    standard : "XI",
+    contactNumber : 4152556
+  },
+  {
+    firstName : "mr.abc",
+    lastName : "yzx",
+    rollNumber : 414552,
+    standard : "XI",
+    contactNumber : 4152556
+  }
+];
+
+// Function - It is a block of code 
+// syntax 
+// function function_name(){
+//   code logic
+// }
+
+// function_name - Name of the block (Allowed character for function name a-z, A-Z, _ , $)
+
+function welcomeMessage(){
+  var message = "Hello! Good Day";
+  console.log(message);
+  alert(message);
+}
