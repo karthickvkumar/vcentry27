6bd5ddab22260b1dace2f1dc7a3600074938854b
karthickvkumar <karthickvelraj@live.in>
2023-10-28 12:48:51 +0530

    commit message

---
+++ learning-react/src/App.js
@@ -0,0 +1,12 @@
+import HomeScreen from "./screens/home-page";
+
+function App(){
+  return(
+    <div>
+      <h1>Hello! Welcome to React JS</h1>
+      <HomeScreen></HomeScreen>
+    </div>
+  )
+}
+
+export default App;
---
+++ learning-react/src/screens/home-page.js
@@ -0,0 +1,29 @@
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default HomeScreen;
+
+// ES5
+// function ComponentName(){
+
+// }
+// export default ComponentName;
+
+
+// ES6 - ES2015
+// const ComponentName = () => {
+
+// }
+// expect default ComponentName;
+
+// Naming Convention 
+
+// 1. Pascal Case - KarthickKumar (Component Name)
+// 2. Camel Case - karthickKumar (Variable or Function Name)
+// 3. Snake Case - karthick_kumar (Variable or Function Name)
+// 4. Kebab Case - karthick-kumar (HTML Attribute and CSS Class)
