a29d9002a09678aad7c7691f8426c4a1fdde6c15
karthickvkumar <karthickvelraj@live.in>
2023-10-29 12:56:32 +0530

    commit message

--- learning-react/src/screens/login-page.js
+++ learning-react/src/screens/login-page.js
@@ -1,11 +1,28 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
+import "../css/login.css";
+
 const LoginScreen = () => {
+  
+  const message = "Welcome to React";
+
+  const welcomeMessage = () => {
+    alert("Welcome to React");
+  }
+
+  const captureEvent = (event) => {
+    console.log(event);
+  }
+  
   return(
     <div>
-      <h1>Login page</h1>
+      <h1 className="heading">Login page {message}</h1>
       <NavLink to="/home">Go to Home Screen</NavLink>
+      <button onClick={() => welcomeMessage()} >Print Message</button>
+      <br></br>
+      <label>Enter your Message</label>
+      <input type="text" placeholder="Enter your message" onChange={captureEvent} />
     </div>
   )
 }
