58d8efa3a2e983b38a047c24ddced49b3d87226a
karthickvkumar <karthickvelraj@live.in>
2023-10-29 11:23:01 +0530

    commit message

--- learning-react/src/screens/home-page.js
+++ learning-react/src/screens/home-page.js
@@ -1,7 +1,11 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
 const HomeScreen = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <NavLink to="/contact">Move to Contact Page</NavLink>
     </div>
   )
 }
--- learning-react/src/screens/login-page.js
+++ learning-react/src/screens/login-page.js
@@ -1,7 +1,11 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
 const LoginScreen = () => {
   return(
     <div>
       <h1>Login page</h1>
+      <NavLink to="/home">Go to Home Screen</NavLink>
     </div>
   )
 }
