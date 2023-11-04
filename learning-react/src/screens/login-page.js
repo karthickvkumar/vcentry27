1ff8b61ad9e75b8afd14945d20c6d73851984e3a
karthickvkumar <karthickvelraj@live.in>
2023-11-04 12:15:15 +0530

    commit message

--- learning-react/src/screens/login-page.js
+++ learning-react/src/screens/login-page.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import {NavLink} from "react-router-dom";
 
 import "../css/login.css";
 
 const LoginScreen = () => {
   
+  // const [variableName, functionName] = useState(initialValue)
+  const [text, updateText] = useState("");
+  
   let message = "Good Morning!";
   let greeting = "Hello, Welcome"; 
 
   const displayMessage = () => {
-    alert(greeting);
+    // alert(greeting);
+    alert(text);
   }
 
   const getValueFromInput = (event) => {
-    console.log("Im working");
+    // console.log("Im working");
     console.log(event.target.value);
+
+    updateText(event.target.value);
   }
 
   return(
@@ -30,6 +36,7 @@ const LoginScreen = () => {
       </div>
       <h2>{message}</h2>
       <h2>{greeting}</h2>
+      <h1>{text}</h1>
       <button onClick={() => displayMessage()}>Greeting Message</button>
     </div>
   )
