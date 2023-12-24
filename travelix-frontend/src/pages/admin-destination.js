dff0a2edb59ba75ef998de1a4f7d0d6e7a4e3875
karthickvkumar <karthickvelraj@live.in>
2023-12-24 11:21:54 +0530

    commit

--- travelix-frontend/src/pages/admin-destination.js
+++ travelix-frontend/src/pages/admin-destination.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const AdminDestinationPage = () => {
+
+  const [destinationForm, updateDestinationForm] = useState({
+    destinationName : "",
+    location : "",
+    destinationImage : "",
+    destinationCount : ""
+  });
+
+
+  const getValuefromInput = (event) => {
+    updateDestinationForm({...destinationForm, [event.target.id] : event.target.value});
+  }
+
+  const uploadDestination = () => {
+    console.log(destinationForm);
+  }
+
+  const uploadImage = (event) => {
+    // console.log(event.target.files)
+    const reader = new FileReader();
+    const file = event.target.files[0];
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      updateDestinationForm({...destinationForm, "destinationImage" : reader.result});
+    }
+  }
+
   return (
     <div>
       <div className='container-fluid'>
       <h2>Upload Destination Details</h2>
         <div>
           <label>Enter destination name :</label>
-          <input type='text' placeholder='Destination Name' />
+          <input type='text' id="destinationName" placeholder='Destination Name' onChange={getValuefromInput}/>
           {false && <span className='error-msg'>Missing Destination Name</span>}
         </div>
         <div>
           <label>Enter loacation / area :</label>
-          <input type='text' placeholder='Location/Area' />
+          <input type='text' id="location" placeholder='Location/Area' onChange={getValuefromInput}/>
           { false && <span className='error-msg'>Missing Destination Name</span>}
         </div>
         <div>
           <label>Enter destination image :</label>
-          <input type='file' accept='.png, .jpeg, .jpg'/>
+          <input type='file' id="destinationImage" accept='.png, .jpeg, .jpg' onChange={uploadImage}/>
           {false && <span className='error-msg'>Missing Destination Image</span>}
         </div>
         <div>
           <label>Enter destination count :</label>
-          <input type='text' placeholder='Destination Count' />
+          <input type='text' id="destinationCount" placeholder='Destination Count' onChange={getValuefromInput} />
           {false && <span className='error-msg'>Missing Destination Image</span>}
         </div>
-        <button>{true ? "Upload Destination" : "Loading..."}</button>
+        <button onClick={() => uploadDestination()}>{true ? "Upload Destination" : "Loading..."}</button>
 
       </div>
     </div>
